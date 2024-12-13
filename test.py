074fa48363c254659eaea25ddb6c3705c80fe09c
moonite1209 <moonite1209@qq.com>
2024-12-13 17:18:08 +0800

    pick image

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import torch
 from torchvision.utils import save_image
 import os
+import shutil
 
 def sam_masks_rgb():
     spath = 'data/temp/shenyang/sam_masks'
@@ -18,10 +19,20 @@ def sam_masks_rgb():
             segmentmap[mask]=idx
         save_image(colormap[segmentmap].permute(2,0,1).cpu(), os.path.join(dpath, masks_name.split('.')[0]+'.jpg'))
 
-
+def pick_image():
+    from scene.colmap_loader import read_extrinsics_binary, read_intrinsics_binary, read_points3D_binary
+    # sparse_path = '/mnt/d/BaiduNetdiskDownload/nanfeng/分块sfm数据/chunks/layer_3/chunk_003_001/sparse/0/'
+    sparse_path = 'data/temp/nanfeng/sparse/0/'
+    image_path = '/mnt/d/BaiduNetdiskDownload/nanfeng/图像集/images/'
+    dst_path = 'data/temp/nanfeng/images'
+    images = read_extrinsics_binary(os.path.join(sparse_path, 'images.bin'))
+    cameras = read_intrinsics_binary(os.path.join(sparse_path, 'cameras.bin'))
+    for image in images.values():
+        shutil.copy(os.path.join(image_path, image.name), dst_path)
 
 def main():
-    sam_masks_rgb()
+    # sam_masks_rgb()
+    pick_image()
 
 if __name__ =='__main__':
     main()
