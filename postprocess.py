9da48b1101962a7c0408a25ccacc365c2372069e
moonite1209 <moonite1209@qq.com>
2025-05-28 20:41:12 +0800

    fix feature distance

--- postprocess.py
+++ postprocess.py
@@ -102,10 +102,10 @@ def get_hybird_sim(a, b):
     return args.feature_ratio * feature_sim + (1-args.feature_ratio) * std_xyz_sim
 clusterer = HDBSCAN(min_cluster_size=10, cluster_selection_epsilon=0.01, allow_single_cluster = False, metric='precomputed', core_dist_n_jobs=-1) # HDBSCAN
 
-sampled_point_features_sim = torch.clamp(torch.einsum('ac,bc -> ab', sampled_point_features, sampled_point_features)/2+0.5, 0, 1)
-sampled_std_point_xyz_sim = torch.clamp(torch.exp(-torch.norm(sampled_std_point_xyz[:,None,:] - sampled_std_point_xyz[None,:,:], dim=-1)), 0, 1)
-sampled_hybird_sim = args.feature_ratio*sampled_point_features_sim + (1-args.feature_ratio)*sampled_std_point_xyz_sim
-cluster_labels = clusterer.fit_predict(sampled_hybird_sim.numpy().astype(np.float64))
+sampled_point_features_distance = torch.clamp(1-torch.einsum('ac,bc -> ab', sampled_point_features, sampled_point_features), 0)
+sampled_std_point_xyz_distance = torch.clamp(torch.norm(sampled_std_point_xyz[:,None,:] - sampled_std_point_xyz[None,:,:], dim=-1), 0)
+sampled_hybird_distance = args.feature_ratio*sampled_point_features_distance + (1-args.feature_ratio)*sampled_std_point_xyz_distance
+cluster_labels = clusterer.fit_predict(sampled_hybird_distance.numpy().astype(np.float64))
 
 feature_cluster_centers = torch.zeros(len(np.unique(cluster_labels)) - 1, point_features.shape[-1])
 xyz_cluster_centers = torch.zeros(len(np.unique(cluster_labels)) - 1, point_xyz.shape[-1])
@@ -115,7 +115,7 @@ for i in np.unique(cluster_labels):
     feature_cluster_centers[i] = sampled_point_features[cluster_labels == i].mean(dim = 0)
     xyz_cluster_centers[i] = sampled_std_point_xyz[cluster_labels == i].mean(dim = 0)
 
-point_features_sim = torch.clamp(torch.einsum('ac,bc->ab', point_features, feature_cluster_centers), -1, 1)
+point_features_sim = torch.clamp(torch.einsum('ac,bc->ab', point_features, feature_cluster_centers)/2+0.5, 0, 1)
 std_point_xyz_sim = torch.clamp(torch.exp(-torch.norm(std_point_xyz[:,None,:] - xyz_cluster_centers[None,:,:], dim=-1)), 0, 1)
 hybird_sim = args.feature_ratio*point_features_sim + (1-args.feature_ratio)*std_point_xyz_sim
 confidence = torch.softmax(hybird_sim*10, dim=-1)
